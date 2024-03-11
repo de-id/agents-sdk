b07bcb37532b5f90d2471f6c7773e0a2fe57c676
Ofek Simhi <158498125+osimhi213@users.noreply.github.com>
2024-03-11 17:40:42 +0200

    adding metadata to Agent inteface (#21)
    * adding branded to Agent interface
    
    * show_footer_branded -> branded
    
    * ->is_branded_view
    
    * ->branded
    
    * add planGroup interface to agents-sdk
    
    * typo
    
    ---------
    
    Co-authored-by: Ofek Simhi <ofek.simhi@deidentification.co>

--- src/types/entities/agents/agent.ts
+++ src/types/entities/agents/agent.ts
@@ -3,6 +3,26 @@ import { Knowledge } from './knowledge';
 import { LLM } from './llm';
 import { Presenter } from './presenter';
 
+// @deprecated - only for backwards compatibility
+export enum UserPlan {
+    TRIAL = 'trial',
+    BASIC = 'basic',
+    ENTERPRISE = 'enterprise',
+    LITE = 'lite',
+    ADVANCED = 'advanced',
+}
+
+export enum PlanGroup {
+    TRIAL = 'deid-trial',
+    PRO = 'deid-pro',
+    ENTERPRISE = 'deid-enterprise',
+    LITE = 'deid-lite',
+    ADVANCED = 'deid-advanced',
+    BUILD = 'deid-api-build',
+    LAUNCH = 'deid-api-launch',
+    SCALE = 'deid-api-scale',
+}
+
 export interface Agent {
     id: string;
     username?: string;
@@ -13,6 +33,7 @@ export interface Agent {
     tags?: string[];
     chats?: number;
     access?: 'private' | 'pending-public' | 'unlisted' | 'rejected' | 'public';
+    metadata?: { plan: PlanGroup | UserPlan };
 
     preview_name?: string;
     preview_description?: string;
@@ -20,7 +41,10 @@ export interface Agent {
     preview_url?: string;
 }
 
-export type AgentPayload = Omit<Agent, 'type' | 'created_at' | 'modified_at' | 'id' | 'owner_id' | 'idle_video_url'>;
+export type AgentPayload = Omit<
+    Agent,
+    'type' | 'created_at' | 'modified_at' | 'id' | 'owner_id' | 'metadata' | 'idle_video_url'
+>;
 
 export interface AgentsAPI {
     create(payload: AgentPayload, options?: RequestInit): Promise<Agent>;
