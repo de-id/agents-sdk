1c30d8f5856829c63794f17d0e6f1d5b30872874
Daniel Mortman <danielmortman@gmail.com>
2024-05-20 16:58:52 +0300

    Call error on stream rejected

--- src/createAgentManager.ts
+++ src/createAgentManager.ts
@@ -189,17 +189,28 @@ export async function createAgentManager(agent: string, options: AgentManagerOpt
                     analytics.track('agent-message-received', { messages: items.messages.length });
                 }
                 options.callbacks.onNewMessage?.(items.messages);
-            } else if (
-                [
-                    StreamEvents.StreamVideoCreated,
-                    StreamEvents.StreamVideoDone,
-                    StreamEvents.StreamVideoError,
-                    StreamEvents.StreamVideoRejected,
-                ].includes(event as StreamEvents)
-            ) {
-                // Stream video event
-                const streamEvent = event.split('/')[1];
-                analytics.track('agent-video', { ...data, event: streamEvent });
+            } else {
+                if (
+                    [
+                        StreamEvents.StreamVideoCreated,
+                        StreamEvents.StreamVideoDone,
+                        StreamEvents.StreamVideoError,
+                        StreamEvents.StreamVideoRejected,
+                    ].includes(event as StreamEvents)
+                ) {
+                    // Stream video event
+                    const streamEvent = event.split('/')[1];
+                    analytics.track('agent-video', { ...data, event: streamEvent });
+                }
+                if (
+                    [
+                        StreamEvents.StreamFailed,
+                        StreamEvents.StreamVideoError,
+                        StreamEvents.StreamVideoRejected,
+                    ].includes(event as StreamEvents)
+                ){
+                    options.callbacks.onError?.(new Error(`Stream failed with event ${event as StreamEvents}`), { data });
+                }
             }
         },
     };
