1980597d73225357b09e315456fe7cb619f22970
almog100 <almog.gal@deidentification.co>
2025-02-17 15:50:55 +0200

    dvp dev agent

--- demo/environment.ts
+++ demo/environment.ts
@@ -2,4 +2,5 @@ export const nodeEnv = 'staging';
 export const didApiUrl = 'https://api-dev.d-id.com';
 export const didSocketApiUrl = 'wss://notifications-dev.d-id.com';
 export const mixpanelKey = '79f81a83a67430be2bc0fd61042b8faa';
-export const agentId = 'v2_agt_lNCi-998';
+export const agentId = 'v2_agt_L9pNlVey';
+// export const agentId = 'v2_agt_lNCi-998';
