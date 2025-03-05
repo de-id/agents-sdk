57dabeb9571e8ed14d2fc0e30f19a102f3d81c8e
Sagi Hammer <sagih95@gmail.com>
2025-03-05 16:20:39 +0200

    remove

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,5 +1,4 @@
 export * from './chat/chat-creation-failed';
 export * from './chat/chat-mode-downgraded';
-export * from './network-error';
 export * from './validation-error';
 export * from './ws-error';
