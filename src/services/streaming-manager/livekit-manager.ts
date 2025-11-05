001718733956e29de81e34159e070d3a75578b92
Ofek Simhi <ofek.simhi@deidentification.co>
2025-11-05 11:55:50 +0200

    Bugfix stransport provider

--- src/services/streaming-manager/livekit-manager.ts
+++ src/services/streaming-manager/livekit-manager.ts
@@ -160,7 +160,7 @@ export async function createLiveKitStreamingManager<T extends CreateStreamOption
 
     try {
         const streamResponse = await streamApi.createStream({
-            transport: Transport.Livekit,
+            transport_provider: Transport.Livekit,
         });
 
         const { agent_id, session_id, session_token: token, session_url: url } = streamResponse;
