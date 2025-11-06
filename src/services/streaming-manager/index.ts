12706a98bff57a4278b90f674c93838aeb36ac86
Ofek Simhi <ofek.simhi@deidentification.co>
2025-11-06 15:22:28 +0200

    Transport -> TransportProvider

--- src/services/streaming-manager/index.ts
+++ src/services/streaming-manager/index.ts
@@ -1,4 +1,4 @@
-export { StreamVersion, createStreamingManager } from './factory';
+export { StreamApiVersion as StreamVersion, createStreamingManager } from './factory';
 export type { ExtendedStreamOptions } from './factory';
 
 export type { StreamingManager } from './common';
