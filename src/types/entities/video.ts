22c556489d635629f422e440cf3cb1a1ca4aee47
Arik Sfaradi <arik.sfaradi@d-id.com>
2025-12-17 12:05:23 +0200

    unuse

--- src/types/entities/video.ts
+++ src/types/entities/video.ts
@@ -3,14 +3,3 @@ export enum VideoType {
     Talk = 'talk',
     Expressive = 'expressive',
 }
-
-export const mapVideoType = (type: string): VideoType => {
-    switch (type) {
-        case 'clip':
-            return VideoType.Clip;
-        case 'talk':
-            return VideoType.Talk;
-        default:
-            throw new Error(`Unknown video type: ${type}`);
-    }
-};
