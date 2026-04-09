0514f79f9d28c8e5f5143c67d8276345d7750f95
omrizitrin-did <omri.zitrin@deidentification.co>
2026-04-09 11:29:57 +0300

    fix index

--- src/services/agent-manager/index.ts
+++ src/services/agent-manager/index.ts
@@ -1,4 +1,5 @@
 import {
+    Agent,
     AgentManager,
     AgentManagerOptions,
     Chat,
@@ -82,7 +83,9 @@ export async function createAgentManager(agent: string, options: AgentManagerOpt
     const agentsApi = createAgentsApi(options.auth, baseURL, options.callbacks.onError, options.externalId);
 
     const agentEntity = await agentsApi.getById(agent);
-    options.debug = options.debug || agentEntity.advanced_settings?.ui_debug_mode;
+    options.debug =
+        options.debug ||
+        (agentEntity as Agent & { advanced_settings?: { ui_debug_mode?: boolean } })?.advanced_settings?.ui_debug_mode;
 
     const isStreamsV2 = isStreamsV2Agent(agentEntity.presenter.type);
     analytics.enrich(getAgentInfo(agentEntity));
